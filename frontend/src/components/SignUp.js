54d1fd9102b53d0f12e89d8460810cfc790304b6
shubhra tiwary <shubhratiwary5@gmail.com>
2024-07-06 19:14:43 +0530

    fetching the authentication requests

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -4,6 +4,22 @@ const SignUp = () => {
     const [username,setUsername]=useState('')
     const [email,setEmail]=useState('')
     const [password,setPassword]=useState('')
+    async function handleSubmit(){
+        const response = await fetch('http://localhost:5000/api/signup',{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({username,email,password}),
+            credentials:true,
+           })
+           if(response.ok){
+            alert('user created ')
+           }else{
+            alert('not created')
+           }
+        
+    }
   return (
     <div className='flex flex-col w-full h-screen justify-center items-center  '>
         <div className='border-2 border-black p-5 flex flex-col gap-10'>
@@ -12,7 +28,7 @@ const SignUp = () => {
         <input type='text' placeholder='username' onChange={(e)=>setUsername(e.target.value)} className='border-2 border-black p-3'></input>
         <input type='email' placeholder='email' onChange={(e)=>setEmail(e.target.value)} className='border-2 border-black p-3'></input>
         <input type='password' placeholder='password' onChange={(e)=>setPassword(e.target.value)} className='border-2 border-black p-3'></input>
-        
+        <button onClick={handleSubmit}>submit</button>
         </form>
         </div>
         
