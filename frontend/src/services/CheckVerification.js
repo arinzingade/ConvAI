ea5846a72ed652c69e6dcc624471e0c5ee2a9570
shubhra tiwary <shubhratiwary5@gmail.com>
2024-07-09 05:26:59 +0530

    updated the fetch request

--- frontend/src/services/CheckVerification.js
+++ frontend/src/services/CheckVerification.js
@@ -1,6 +1,6 @@
 export const CheckVerification = async ()=>{
     try{
-        const response = await fetch('http://localhost:5000/api/protected',{
+        const response = await fetch('http://127.0.0.1:5000/api/protected',{
             method:'GET',
             credentials:'include'
         })
