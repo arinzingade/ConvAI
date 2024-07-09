37edba11ee8a5044e4107649878842cd1f010f7c
shubhra tiwary <shubhratiwary5@gmail.com>
2024-07-09 07:09:04 +0530

    homepage style

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -8,22 +8,25 @@ export const HomePage = () => {
     const tokenVerified =useStore((state)=>state.tokenVerified)
   return (
     <>
-        <Navbar></Navbar>
+    <div className='w-screen h-screen'>
+    <Navbar></Navbar>
         {tokenVerified ? 
   ( <div>
     you are logged in
     <CharactersList></CharactersList>
     </div>):
        (
-        <div className='w-full mt-20 text-4xl flex flex-col justify-center items-center text-[#6C757D]'>
-        <p>Login to view various characters to
-          <span className='text-4xl text-blue-400'> Interact</span>  </p>
-          <div className='m-0'>
-            <img src={image} className='max-h-full'></img>
+        <div className='w-full h-full text-4xl flex flex-col justify-center items-center text-[#6C757D] '>
+        <p>Login to view various characters and
+          <span className='text-5xl text-blue-400'> Interact</span>  </p>
+          <div  className='w-full flex justify-center'>
+            <img src={image} className='max-h-72 object-contain'></img>
           </div>
         </div>
        )
        }
+    </div>
+    
     </>
   )
 }
