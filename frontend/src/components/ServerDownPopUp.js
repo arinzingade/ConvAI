79c26f70ef16161f87dad18be0b5f0d5aad18e08
shubhra tiwary <shubhratiwary5@gmail.com>
2024-07-09 13:24:41 +0530

    styling for the serverDownPopUP

--- frontend/src/components/ServerDownPopUp.js
+++ frontend/src/components/ServerDownPopUp.js
@@ -6,10 +6,13 @@ const ServerDownPopup = ({ isVisible,onclose}) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-900 bg-opacity-50">
-      <div className="bg-white p-4 rounded shadow-lg">
-        <h2>Server Down</h2>
-        <p>It looks like the server is currently down. Please try again later.</p>
-        <button onClick={onclose}>close</button>
+      <div className="bg-red-400 w-1/2 h-1/3 p-5 rounded-xl shadow-lg flex flex-col justify-around items-center">
+      <div className="w-full text-xl">
+      <h2>Server Down</h2>
+      <p>It looks like the server is currently down. Please try again later.</p>
+      </div>
+      
+        <button onClick={onclose} className="border-2 border-black w-1/4">close</button>
       </div>
 
     </div>
