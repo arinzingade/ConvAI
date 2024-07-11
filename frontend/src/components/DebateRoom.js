7f01c66a734699f611c41dc72c28da8f4f4ed485
shubhra tiwary <shubhratiwary5@gmail.com>
2024-07-11 12:34:07 +0530

    styles for the debateroom

--- frontend/src/components/DebateRoom.js
+++ frontend/src/components/DebateRoom.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const DebateRoom = () => {
   return (
-    <div className='w-screen h-screen bg-slate-200 flex'>
-        <div className='w-1/4 h-full bg-white shadow-lg shadow-black'></div>
+    <div className='w-screen h-screen bg-slate-100 flex'>
+        <div className='w-1/4 h-full bg-white shadow-lg '></div>
         <div className='w-3/4 h-full flex justify-center items-end'>
             <div className='w-3/4  sticky bottom-10 '>
-                <input type='text' className='w-full shadow-2xl shadow-black rounded-2xl  py-10 '></input>
+                <input type='text' className='w-full shadow-2xl  rounded-2xl  py-10 '></input>
             </div>
         </div>
     </div>
