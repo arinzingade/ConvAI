1b23c0cd71cc111a1d48b706c6f85749d89035b8
shubhra tiwary <shubhratiwary5@gmail.com>
2024-07-27 22:42:24 +0530

    fetching the list and mapping over it

--- frontend/src/components/CharacterList.js
+++ frontend/src/components/CharacterList.js
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { FetchingCharacters } from '../services/CharacterServices'
 const CharacterList = () => {
+  const [list,setList]=useState([])
+  useEffect(()=>{
+   async function fetchList(){
+      const response = await FetchingCharacters()
+      setList(response)
+    }
+    fetchList()
+  })
   return (
     <div>
-        
+        {list.map((element)=>{
+          return(
+            <>
+              {element}
+            </>
+          )
+        })}
     </div>
   )
 }
