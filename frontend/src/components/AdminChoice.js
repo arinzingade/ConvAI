2a957e1e4423aa6f973d3e25753ad3c89a9511d1
shubhra tiwary <shubhratiwary5@gmail.com>
2024-07-29 12:20:35 +0530

    character form updated

--- frontend/src/components/AdminChoice.js
+++ frontend/src/components/AdminChoice.js
@@ -6,11 +6,14 @@ export const AdminChoice = () => {
     function handleSubmit(){
         navigate('/choice/form')
     }
+    function handleUpdate(){
+        navigate('/adminPanel')
+    }
   return (
   <div className='w-screen h-screen'>
     <div className='h-1/2 flex justify-center items-center gap-20'>
         <button className='border  px-10 py-2 border-black' onClick={handleSubmit}>create New character</button>
-        <button className='border  px-10 py-2 border-black'>Update  character</button>
+        <button className='border  px-10 py-2 border-black' onClick={handleUpdate}>Update  character</button>
     </div>
   </div>
   )
--- frontend/src/components/CharacterForm.js
+++ frontend/src/components/CharacterForm.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CharacterForm = () => {
+    const [name,setName]=useState()
+  async  function handleSubmit(e){
+        e.preventDefault()
+        try{
+            const response = await fetch('/createChar',{
+                method:'POST',
+                body:name,
+                credentials:'include'
+            })
+            if(response.ok){
+                console.log('character Created')
+            }
+            
+        }catch(err){
+            console.log(Error)
+        }
+    }
   return (
     <div>
         <form>
-            <input type='text' placeholder='name of the character'></input>
-            <input type='text' placeholder='name of the character'></input>
-            <input type='text' placeholder='name of the character'></input>
+            <input type='text' placeholder='name of the character' onChange={(e)=>setName(e.target.value)}></input>
+            <button onClick={handleSubmit}></button>
         </form>
     </div>
   )
